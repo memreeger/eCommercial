ea6f84e8b8603720f323d63eef83ed5b01f2277b
Muhammed Emre Eğer <m.emre.eger@gmail.com>
2025-12-29 22:18:46 +0300

    chore: remove unused plugins from Vite config and add Netlify configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
-import { reactRouter } from "@react-router/dev/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss(), reactRouter(),
-  tsconfigPaths(),
-  netlifyReactRouter()],
+  plugins: [react(), tailwindcss(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
